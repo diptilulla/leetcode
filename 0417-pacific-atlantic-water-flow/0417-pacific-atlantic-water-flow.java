0b5332398869ecc696abb1af38c566d653d26ac5
DIPTI LULLA <60360333+diptilulla@users.noreply.github.com>
2025-10-07 01:30:41 +0530

    Time: 11 ms (33.35%), Space: 45.4 MB (91.76%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,4 +1,5 @@
 class Solution {
+    static int[][] dirs={{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         Queue<int[]> q1 = new ArrayDeque<>();
         Queue<int[]> q2 = new ArrayDeque<>();
@@ -19,30 +20,13 @@ class Solution {
             q2.add(new int[]{n-1, j});
             vis2[n-1][j] = true;
         }
-        int[][] dirs={{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        while(!q1.isEmpty()) {
-            int[] item= q1.poll();
-            for(int[] dir:dirs) {
-                if(isValid(item[0]+dir[0], item[1]+dir[1], n, m) && !vis1[item[0]+dir[0]][item[1]+dir[1]] && heights[item[0]+dir[0]][item[1]+dir[1]]>=heights[item[0]][item[1]]) {
-                    vis1[item[0]+dir[0]][item[1]+dir[1]]=true;
-                    q1.add(new int[]{item[0]+dir[0],item[1]+dir[1]});
-                }
-            }
-        }
-        while(!q2.isEmpty()) {
-            int[] item= q2.poll();
-            for(int[] dir:dirs) {
-                if(isValid(item[0]+dir[0], item[1]+dir[1], n, m) && !vis2[item[0]+dir[0]][item[1]+dir[1]] && heights[item[0]+dir[0]][item[1]+dir[1]]>=heights[item[0]][item[1]]) {
-                    vis2[item[0]+dir[0]][item[1]+dir[1]]=true;
-                    q2.add(new int[]{item[0]+dir[0],item[1]+dir[1]});
-                }
-            }
-        }
+        bfs(q1, vis1, heights, n, m);
+        bfs(q2, vis2, heights, n, m);
 
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(vis1[i][j] && vis2[i][j]) {
-                    result.add(new ArrayList<>(Arrays.asList(i, j)));
+                    result.add(Arrays.asList(i, j));
                 }
             }
         }
@@ -55,5 +39,17 @@ class Solution {
             j>=0 && j<m)
             return true;
         return false;
+    }
+
+    public void bfs(Queue<int[]> q, boolean[][] vis, int[][] heights, int n, int m) {
+        while(!q.isEmpty()) {
+            int[] item= q.poll();
+            for(int[] dir:dirs) {
+                if(isValid(item[0]+dir[0], item[1]+dir[1], n, m) && !vis[item[0]+dir[0]][item[1]+dir[1]] && heights[item[0]+dir[0]][item[1]+dir[1]]>=heights[item[0]][item[1]]) {
+                    vis[item[0]+dir[0]][item[1]+dir[1]]=true;
+                    q.add(new int[]{item[0]+dir[0],item[1]+dir[1]});
+                }
+            }
+        }
     }
 }
